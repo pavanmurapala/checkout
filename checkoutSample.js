878d8ba4346aafe8f4406c625cc2fc4e6d6193f3
pavanmurapala <pmurapala@virtusa.com>
2020-10-22 11:52:21 +0530

    Updates

---
+++ checkoutSample.js
@@ -0,0 +1,51 @@
+
+
+function errorCallback(error) {
+    console.log(JSON.stringify(error));
+}
+function cancelCallback() {
+    console.log('Payment cancelled');
+}
+function paymentSuccess() {
+    alert("Payment is successfull")
+}
+function configureCheckout(sessionID, address, type) {
+    Checkout.configure({
+        session: {
+            id: sessionID
+        },
+        interaction: {
+            merchant: address
+        }
+    }).then((e) => {
+        if (type == "lightBox") Checkout.showLightbox();
+        else Checkout.showPaymentPage();
+    });
+}
+async function fectchSessionInfo() {
+    /*  fetch('https://ap-gateway.mastercard.com/api/nvp/version/57/')
+         .then(response => response.json())
+         .then(data => console.log(data)); */
+
+    axios.post('https://ap-gateway.mastercard.com/api/rest/version/57/merchant/TESTMETLIFENI01/session', {
+        "apiOperation": "CREATE_CHECKOUT_SESSION",
+        "interaction": {
+            "operation": "PURCHASE"
+        },
+        "order": {
+            "amount": "120.0",
+            "currency": "USD",
+            "description": "Ordered goods",
+            "id": "232E32323ddd"
+        }
+    },
+        { crossDomain: true })
+        .then(function (response) {
+            return response;
+        })
+        .catch(function (error) {
+            return error;
+        });
+}
+
+//window.addEventListener('load', configureCheckout)
---
+++ precache-manifest.7135567b54baae26b187a8865c80d9e3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a6ddf13bff190db30b8422b0432e195",
+    "url": "/checkout/index.html"
+  },
+  {
+    "revision": "a7d395a15db554e9be99",
+    "url": "/checkout/static/css/2.6414bb7b.chunk.css"
+  },
+  {
+    "revision": "83dfb3e1139bb400e061",
+    "url": "/checkout/static/css/main.23658bbd.chunk.css"
+  },
+  {
+    "revision": "a7d395a15db554e9be99",
+    "url": "/checkout/static/js/2.acc62521.chunk.js"
+  },
+  {
+    "revision": "8a137f586fcebb1a370343eb47b7677c",
+    "url": "/checkout/static/js/2.acc62521.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "83dfb3e1139bb400e061",
+    "url": "/checkout/static/js/main.551b9d18.chunk.js"
+  },
+  {
+    "revision": "15bab60db3c38a25621e",
+    "url": "/checkout/static/js/runtime-main.9b2734c8.js"
+  }
+]);
