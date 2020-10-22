8032b238b0ec00fdca29d38ea259dde205f0fc1b
pavanmurapala <pmurapala@virtusa.com>
2020-10-22 12:31:21 +0530

    AUTHORIZE value added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,122 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import { Container, Row, Col, Form, FormGroup, Input, Label } from 'reactstrap';
 
 function App() {
+  let { Checkout } = window;
+  let { fectchSessionInfo, configureCheckout } = window;
+  let [session, setSession] = useState('');
+  const gotoLightBox = () => {
+
+    configureCheckout(session.session.id, {
+      name: 'Some merchant name',
+      address: {
+        line1: '200 Sample St',
+        line2: '1234 Example Town'
+      }
+    }, "lightBox")
+
+  }
+  const showPaymentPage = () => {
+    configureCheckout(session.session.id, {
+      name: 'Some merchant name',
+      address: {
+        line1: '200 Sample St',
+        line2: '1234 Example Town'
+      }
+    }, "")
+  }
+  const byNow = async (e) => {
+    e.preventDefault();
+
+    session = await fectchSessionInfo()
+    /*  session = setSession({
+       "merchant": "TESTMETLIFENI01",
+       "result": "SUCCESS",
+       "session": {
+         "id": "SESSION0002849411191K3702312H28",
+         "updateStatus": "SUCCESS",
+         "version": "56ddf2a301"
+       },
+       "successIndicator": "33e4beca4b01436b"
+     }) */
+  }
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Container>
+          <Row><Col><p className="checkout" >Store</p></Col></Row>
+          {session.result === "SUCCESS" ?
+
+            <Row>
+              <Col xs="6" sm="6" md="6" className="billingForm">
+                <Form><h2>Proivde Billing Information</h2>
+                  <FormGroup row>
+                    <Label for="exampleEmail" sm={4}>Amount</Label>
+                    <Col sm={8}>
+                      <b >120 $</b>
+                    </Col>
+                  </FormGroup>
+                  <FormGroup row>
+                    <Label for="exampleEmail" sm={4}>Description</Label>
+                    <Col sm={8}>
+                      <b >Ordered goods</b>
+                    </Col>
+                  </FormGroup>
+
+
+                  <FormGroup row>
+                    <Label for="exampleEmail" sm={4}>Email</Label>
+                    <Col sm={8}>
+                      <Input type="text" name="name" id="name" placeholder="Enter Name" />
+                    </Col>
+                  </FormGroup>
+                  <FormGroup row>
+                    <Label for="exampleAddress" sm={4}>Address</Label>
+                    <Col sm={8}>
+                      <Input type="textarea" name="address" id="exampleAddress" placeholder="Address placeholder" />
+                    </Col>
+                  </FormGroup>
+
+                  <FormGroup row>
+                    <Col sm={6}>
+                      <Input type="button" value="Pay with Lightbox" onClick={gotoLightBox} />
+                    </Col>
+                    <Col sm={6}>
+                      <Input type="button" value="Pay with Payment Page" onClick={showPaymentPage} />
+                    </Col>
+                  </FormGroup>
+                </Form>
+
+              </Col></Row> :
+
+            <Row>
+              <Col xs="6" sm="3" md="3"><img className={"shirtImg"} src="https://react-shooping-cart.netlify.app/img/1.jpg?v=1" />
+                <div className={"buy"}>
+                  <p>120 $</p>
+                  <Input type="button" className={"btn"} value="Buy Now" onClick={byNow} /></div>
+              </Col>
+              <Col xs="6" sm="3" md="3"><img className={"shirtImg"} src="https://react-shooping-cart.netlify.app/img/2.jpg?v=2" />
+                <div className={"buy"}>
+                  <p>120 $</p>
+                  <Input type="button" className={"btn"} value="Buy Now" onClick={byNow} /></div></Col>
+              <Col xs="6" sm="3" md="3"><img className={"shirtImg"} src="https://react-shooping-cart.netlify.app/img/3.jpg?v=3" />
+                <div className={"buy"}>
+                  <p>120 $</p>
+                  <Input type="button" className={"btn"} value="Buy Now" onClick={byNow} />
+                </div>
+              </Col>
+            </Row>
+          }
+
+          {/* <div className="" style={{ textAlign: "center" }}>
+          <div className="row">
+          <div className={"col-md-6"}></div>
+          </div>
+          <input type="button" className={"btn"} value="Pay with Lightbox" onClick={gotoLightBox} />
+          <input type="button" className={"btn"} value="Pay with Payment Page" onClick={showPaymentPage} />
+        </div> */}
+        </Container>
       </header>
     </div>
   );
